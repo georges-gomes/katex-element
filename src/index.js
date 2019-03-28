3e341b66280f0dfdfa09407c4c3b253d7b8ee8b3
Georges Gomes <georges.gomes@gmail.com>
2019-03-28 16:24:15 +0100

    moved to lightdom because of style issues

--- src/index.js
+++ src/index.js
@@ -1,45 +1,46 @@
 import {
-  LitElement,
-  html,
-  css
+    LitElement,
+    html,
+    css
 } from 'lit-element';
 import katex from 'katex/dist/katex.mjs';
 
 export default class KatexElement extends LitElement {
 
-  static get properties() {
-      return {
-          displayMode: { type: Boolean },
-      };
-  }
+    static get properties() {
+        return {
+            displayMode: { type: Boolean },
+            exp: {type: String}
+        };
+    }
 
-  constructor()
-  {
-      super();
-      this.displayMode = false;
-  }
+    constructor()
+    {
+        super();
+        this.displayMode = false;
+        this.exp = '';
+    }
 
-  firstUpdated() {
-      this.elSlot = this.shadowRoot.querySelector('slot');
-      let slotNode = this.elSlot.assignedNodes()[0];
-      let slotData = slotNode.data;
+    createRenderRoot() {
+        return this;//.attachShadow({mode: 'open'});
+    }
 
-      let elContainer = this.shadowRoot.querySelector('#container');
-      katex.render(slotData, elContainer, {
-          displayMode: this.displayMode,
-          throwOnError: false
-      });
-  }
+    firstUpdated() {
+        let elContainer = this.querySelector('#container');
+        katex.render(this.exp, elContainer, {
+            displayMode: this.displayMode,
+            throwOnError: false
+        });
+    }
 
-  render() {
-      return html`
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.css" integrity="sha384-dbVIfZGuN1Yq7/1Ocstc1lUEm+AT+/rCkibIcC/OmWo5f0EA48Vf8CytHzGrSwbQ" crossorigin="anonymous">
+    render() {
+        return html`
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.css" integrity="sha384-dbVIfZGuN1Yq7/1Ocstc1lUEm+AT+/rCkibIcC/OmWo5f0EA48Vf8CytHzGrSwbQ" crossorigin="anonymous">
 
-      <div id="container">
-          <slot></slot>
-      </div>
-  `;
-  }
+        <div id="container">
+        </div>
+    `;
+    }
 }
 
 window.customElements.define('katex-element', KatexElement);
